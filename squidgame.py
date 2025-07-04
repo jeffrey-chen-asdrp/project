e436df37fd3ae44b2b8b541cbf04637dc515adb7
hecker40 <jeffrey.chen@students.asdrp.org>
2025-07-04 14:36:07 -0700

    Create player functions
    Added relationship chance function, build relationship function, kill function (class Game), developing function for chance of different attacks

--- squidgame.py
+++ squidgame.py
@@ -3,8 +3,10 @@ import random
 players = [] # list of all players
 
 class Agent:
-  def __init__(self, id, age, gender, morals, physical, mental, debt, stress, trauma, motiv, loyalty):
+  def __init__(self, id, age, gender, morals, physical, mental, debt, stress, motiv, loyalty):
     self.id = id # player identification number
+
+    self.hp = 100 # starts with 100 but recovers over time
 
     self.age = age
     self.gender = gender
@@ -16,45 +18,48 @@ class Agent:
     self.debt = debt # player's debt
 
     self.stress = stress # stress levels
-    self.trauma = trauma # trauma levels
     self.motiv = motiv # motivation
 
     self.loyalty = loyalty # how loyal you are to friends
     # self.similarity_bias = similarity_bias # bias to cooperating with similar people
 
-  def kill(self, id):
-    for player in players:
-      if player.id == id:
-        players.remove(player)
+    self.inventory = [] # can include knives, punch, forks, etc.
 
-        break
+    self.relationships = [] # every player with a relationship would be modeled with [{"player": Agent, "strength": number}]
+    self.voting_history = [] # tracks voting history
 
-    print(f"Player {self.id} has eliminated Player {player.id}")
+  def choose_violence(self, player):
+    # Morals
+    # Physical (difference from your trait and the player's trait)
+    # Age
+    # Motivation
+    
+    # Punch: hp - 10, gain stress, mental decreases, morals decrease, physical decrease, motiv increases
+    # Stab: hp - 35, gain more stress, mental decreases more, morals decrease a lot, physical decreases a lot, motiv decreases a lot
+    # Choke: hp - 100, survival chance 4%, if survives moral decreases, physical increases, stress increase, motiv increases
 
   def relationship_chance(self, player): # returns 0-100
     age_dif = self.age - player.age # less difference equals higher chance
 
-    if self.gender == player.gender:
-      genders = 0
+    if self.gender == player.gender: 
+      gender_dif = 0
 
     else:
-      genders = 15
-    
-    moral_dif = self.morals - player.morals # less difference equals higher chance
-    mental_dif = self.mental - player.mental # less difference equals higher chance
+      gender_dif = 15
 
-    if 40 - abs(age_dif) < genders:
+    if 40 - abs(age_dif) < gender_dif:
       age_gender_var = 0
     
     else:
-      age_gender_var = 40 - abs(age_dif) - genders
+      age_gender_var = 40 - abs(age_dif) - gender_dif
 
+    moral_dif = self.morals - player.morals # less difference equals higher chance
+    mental_dif = self.mental - player.mental # less difference equals higher chance
 
     if 30 - abs(moral_dif) < 0:
       morals_var = 0
 
     morals_var = 30 - abs(moral_dif)
-
 
     if 30 - abs(mental_dif) < 0:
       mental_var = 0
@@ -65,7 +70,52 @@ class Agent:
 
     return chance
   
-Player1 = Agent(1, 36, "Male", 90, 70, 86, 1000, 2, 0, 60, 90)
-Player2 = Agent(2, 51, "Male", 97, 96, 91, 100, 0, 0, 0, 0)
+  def build_relationship(self, player):
+    if not(player in self.relationships):
+      strength = self.relationship_chance(player)
 
-print(Player1.relationship_chance(Player2))
+      self.relationships.append({"player":player, "strength":strength})
+
+      print(f"Player {self.id} has established a relationship with Player {player.id} of strength {strength}")
+
+  def vote(self): # interval of 0-100, high number means higher chance of voting no
+    # morals = 93
+    # 35 * 0.93
+    
+    # stress = 30
+    # 45 * 0.3
+
+    # motiv = 25
+    # 20 * 0.25
+
+    chance = self.morals/100 * 35 + self.stress/100 * 45 + self.motiv/100 * 20
+
+    if random.random() <= chance/100:
+      self.voting_history.append("No")
+      print(f"Player {self.id} has voted no")
+
+      return "No"
+    
+    print(f"Player {self.id} has voted no")
+    self.voting_history.append("Yes")
+
+    return "Yes"
+
+Player1 = Agent(1, 33, "Male", 93, 82, 90, 81, 84, 3, 15, 30)
+Player2 = Agent(2, 41, "Male", 90, 82, 81, 66, 90, 17, 26, 35)
+
+print(Player1.vote())
+
+
+
+"""
+def kill(self, id):
+    for player in players:
+      if player.id == id:
+        players.remove(player)
+
+        print(f"Player {self.id} has eliminated Player {player.id}")
+
+        break
+"""
+#KILL FUNCTION FOR SQUID GAME CLASS AGENT (class Game:)
