4032a3bd7e7804dbe151fcba493a2ce1292aa0fb
hecker40 <jeffrey.chen@students.asdrp.org>
2025-07-04 12:03:28 -0700

    Added setup

---
+++ main.py
@@ -0,0 +1,21 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+client = OpenAI(api_key=API_KEY)
+
+completion = client.chat.completions.create(
+  model="gpt-4.1-mini",
+    messages=[
+      {
+        "role": "user",
+        "content": "What is the capital of France?"
+      }
+  ]
+)
+
+print(completion.choices[0].message.content)
