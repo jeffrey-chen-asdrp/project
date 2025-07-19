8336b85dfa48e957363bf5edaab48e13a1288665
hecker40 <jeffrey.chen@students.asdrp.org>
2025-07-18 23:56:57 -0700

    Model Sky Squid Game

--- squidgame/skysquidgame.py
+++ squidgame/skysquidgame.py
@@ -1,8 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import random
 import time
 
 class Agent:
-  def __init__(self, id, age, gender, physical, charisma, obedience, revenge_tendency, similarity_bias, charisma_bias, indecisiveness, morals, aggression): # all traits 1-100
+  def __init__(self, id, age, gender, physical, charisma, obedience, revenge_tendency, trust_voting, similarity_bias, charisma_bias, indecisiveness, morals, aggression): # all traits 1-100
     self.id = id
 
     self.age = age
@@ -13,6 +15,7 @@ class Agent:
     self.charisma = charisma # more likely for others to follow your vote
     self.obedience = obedience # more likely to vote for majority rather than the player with the lowest relationship strength
     self.revenge_tendency = revenge_tendency # more likely to vote for someone who voted for you (50-100)
+    self.trust_voting = trust_voting # % of trust voting
 
     self.similarity_bias = similarity_bias # (50-100)
     self.charisma_bias = charisma_bias
@@ -35,6 +38,9 @@ class Agent:
     # age and gender = 35%
     # morals = 65%
 
+    # trust 70%
+    # morals 30%
+
     age_diff = abs(self.age - player.age)
     moral_diff = abs(self.morals - player.morals)
 
@@ -47,6 +53,21 @@ class Agent:
     similarity = 100 - 1.2 * (0.35 * (age_diff + gender_diff) + 0.65 * moral_diff)
 
     return similarity
+  
+  def calc_trust_voting(self):
+    players = []
+    values = []
+
+    for relationship in self.relationships:
+      trust = relationship["trust"]
+      morals = relationship["player"].morals
+
+      values.append(0.8 * trust + 0.2 * morals)
+      players.append(relationship["player"])
+
+    index = values.index(min(values))
+
+    return players[index]
   
   def calc_relationship(self, player):
     for relationship in self.relationships:
@@ -82,9 +103,11 @@ class Agent:
         return
       
 class Round:
-  def __init__(self, agents, time):
+  def __init__(self, agents, round_num, time):
     self.agents = agents
     self.voting = [] # {"player":player, "votes":[p2, p3], "voted_for":player}
+    
+    self.round_num = round_num
 
     self.time = time
 
@@ -238,7 +261,7 @@ class Round:
 
             break
 
-        if i == 0: # check if player is first to vote
+        if i == 0 and self.round_num == 1: # check if player is first to vote
           self.vote(player, target)
 
           print(f"Player {player.id} has voted for Player {target.id} due to their lack of similarity")
@@ -253,17 +276,24 @@ class Round:
 
                 break
           
-          if player == self.count_majority(): # revenge 80% similarity 20%
+          if player == self.count_majority(): # revenge 40% trust voting 40% similarity 20%
             # formula = random.choices(population=["P1","P2"], weights=[80*(1-(avg-value))])
-            choice = random.choices(population=["revenge","similarity"], weights=[80*(1-(75-player.revenge_tendency)/100), 20*(1-(75-player.similarity_bias)/100)])
+            choice = random.choices(population=["revenge","trust", "similarity"], weights=[40*(1-(75-player.revenge_tendency)/100), 40*(1-(75-player.trust_voting)/100), 20*(1-(75-player.similarity_bias)/100)])
             
-            if choice[0] == "revenge": # selected revenge 
+            if choice[0] == "revenge" and was_voted: # selected revenge 
               targets = self.get_revenge(player)
               target = random.choice(targets)
 
               self.vote(player, target)
 
               print(f"Player {player.id} has voted for Player {target.id} as an act of revenge")
+
+            elif choice[0] == "trust":
+              target = player.calc_trust_voting()
+
+              self.vote(player, target)
+
+              print(f"Player {player.id} has voted for Player {target.id} due to a lack of trust and morals")
 
             else: # similarity selected
               self.vote(player, target)
@@ -281,17 +311,17 @@ class Round:
 
             if was_voted:
               if has_voted and (player != charisma_target):
-                choice = random.choices(population=["obedience","revenge","similarity","charisma"], weights=[40*(1-(75-player.obedience)/100), 20*(1-(75-player.revenge_tendency)/100), 20*(1-(75-player.similarity_bias)/100), 20*(1-(75-player.charisma_bias)/100)])
+                choice = random.choices(population=["obedience","trust","revenge","similarity","charisma"], weights=[35*(1-(75-player.obedience)/100), 35*(1-(75-player.trust_voting)/100), 15*(1-(75-player.revenge_tendency)/100), 5*(1-(75-player.similarity_bias)/100), 10*(1-(75-player.charisma_bias)/100)])
 
               else:
-                choice = random.choices(population=["obedience","revenge","similarity"], weights=[50*(1-(75-player.obedience)/100), 25*(1-(75-player.revenge_tendency)/100), 20*(1-(75-player.similarity_bias)/100)])
+                choice = random.choices(population=["obedience","trust","revenge","similarity"], weights=[35*(1-(75-player.obedience)/100), 35*(1-(75-player.trust_voting)/100), 20*(1-(75-player.revenge_tendency)/100), 10*(1-(75-player.similarity_bias)/100)])
 
             else:
               if has_voted and (player != charisma_target):
-                choice = random.choices(population=["obedience","similarity","charisma"], weights=[50*(1-(75-player.obedience)/100), 25*(1-(75-player.similarity_bias)/100), 25*(1-(75-player.charisma_bias)/100)])
+                choice = random.choices(population=["obedience","trust","similarity","charisma"], weights=[35*(1-(75-player.obedience)/100), 35*(1-(75-player.trust_voting)/100), 15*(1-(75-player.similarity_bias)/100), 15*(1-(75-player.charisma_bias)/100)])
 
               else:
-                choice = random.choices(population=["obedience","similarity"], weights=[70*(1-(75-player.obedience)/100), 3*(1-(75-player.similarity_bias)/100)])
+                choice = random.choices(population=["obedience","trust","similarity"], weights=[40*(1-(75-player.obedience)/100), 40*(1-(75-player.trust_voting)/100), 20*(1-(75-player.similarity_bias)/100)])
             
             if choice[0] == "obedience":
               target = self.count_majority()
@@ -307,6 +337,13 @@ class Round:
               self.vote(player, target)
 
               print(f"Player {player.id} has voted for Player {target.id} as an act of revenge")
+
+            elif choice[0] == "trust":
+              target = player.calc_trust_voting()
+
+              self.vote(player, target)
+
+              print(f"Player {player.id} has voted for Player {target.id} due to a lack of trust and morals")
 
             elif choice[0] == "similarity": # similarity bias selected
               self.vote(player, target)
@@ -380,7 +417,7 @@ print("Generating agent traits...\n")
 
 time.sleep(0.3)
 
-for i in range(8):
+for i in range(10):
   # id, age, gender, physical, charisma, obedience, revenge_tendency, similarity_bias, charisma_bias, indecisiveness, morals, aggression
 
   age = random.randint(18, 70)
@@ -391,6 +428,7 @@ for i in range(8):
   charisma = random.randint(50, 100)
   obedience = random.randint(50, 100)
   revenge_tendency = random.randint(50, 100)
+  trust_voting = random.randint(50, 100)
   similarity_bias = random.randint(50, 100)
   charisma_bias = random.randint(50, 100)
 
@@ -400,22 +438,22 @@ for i in range(8):
 
   aggression = random.randint(1, 100)
 
-  player = Agent(id, age, gender, physical, charisma, obedience, revenge_tendency, similarity_bias, charisma_bias, indecisiveness, morals, aggression)
+  player = Agent(id, age, gender, physical, charisma, obedience, revenge_tendency, trust_voting, similarity_bias, charisma_bias, indecisiveness, morals, aggression)
   players.append(player)
 
-  print(f"Player:{player.id}, Age:{player.age}, Gender:{player.gender}, Physical:{player.physical}, Charisma:{player.charisma}, Obedience:{player.obedience}, Revenge tendency:{player.revenge_tendency}, Similarity bias:{player.similarity_bias}, Charisma bias:{player.charisma_bias}, Indecisiveness:{player.indecisiveness}, Morals:{player.morals}, Aggression:{player.aggression}")
+  print(f"Player:{player.id}, Age:{player.age}, Gender:{player.gender}, Physical:{player.physical}, Charisma:{player.charisma}, Obedience:{player.obedience}, Revenge tendency:{player.revenge_tendency}, Trust voting:{player.trust_voting}, Similarity bias:{player.similarity_bias}, Charisma bias:{player.charisma_bias}, Indecisiveness:{player.indecisiveness}, Morals:{player.morals}, Aggression:{player.aggression}")
 
   id += 1
 
   time.sleep(0.5)
 
 survivors = players
-game_time = 45
+game_time = 100
 
-for i in range(2):
+for i in range(7):
   print(f"\nRound {i+1} starting...\n")  
 
-  game = Round(players, game_time)
+  game = Round(players, i+1, game_time)
   game.round()
 
   survivors = game.survivors
